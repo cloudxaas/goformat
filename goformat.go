c8695c2c4afbb9c92b7ca719464a1d16593c69b5
HIQ Society <51492452+hiqsociety@users.noreply.github.com>
2023-04-03 07:15:54 -0700

    Update goformat.go

--- goformat.go
+++ goformat.go
@@ -4,6 +4,8 @@ import (
   "github.com/cloudxaas/gocx"
 )
 
+const digits = "0123456789"
+
 func Uint16ToString(num uint16) string {
     if num == 0 {
         return "0"
@@ -16,4 +18,28 @@ func Uint16ToString(num uint16) string {
         num /= 10
     }
     return cx.B2s(buf[i:])
+}
+
+func Int16ToString(n int16) string {
+    if n == 0 {
+        return "0"
+    }
+    
+    var buf [6]byte // maximum length of int16 string is 6 digits
+    i := len(buf)
+    neg := false
+    if n < 0 {
+        neg = true
+        n = -n
+    }
+    for n > 0 {
+        i--
+        buf[i] = digits[int(n%10)]
+        n /= 10
+    }
+    if neg {
+        i--
+        buf[i] = '-'
+    }
+    return string(buf[i:])
 }
