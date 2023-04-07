a23b517e5bdf3919a5094323b19c85bb7cc1b782
Kolin Fluence <kolinfluence@gmail.com>
2023-04-07 00:07:51 -0700

    123

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/cloudxaas/goformat
+
+go 1.20
+
+require (
+	github.com/cloudxaas/gocx v0.0.3
+	github.com/valyala/bytebufferpool v1.0.0
+)
--- goformat.go
+++ goformat.go
@@ -1,7 +1,8 @@
 package cxformat
 
 import (
-  "github.com/cloudxaas/gocx"
+	cx "github.com/cloudxaas/gocx"
+	"github.com/valyala/bytebufferpool"
 )
 
 const digits = "0123456789"
@@ -10,21 +11,22 @@ func Uint16ToString(num uint16) string {
     if num == 0 {
         return "0"
     }
-    buf := [5]byte{} // maximum uint16 digits are 5
-    i := len(buf)
+    buf := bytebufferpool.Get() // get buffer from pool
+    defer bytebufferpool.Put(buf) // return buffer to pool
     for num > 0 {
-        i--
-        buf[i] = byte(num%10) + '0'
+        buf.WriteByte(byte(num%10) + '0')
         num /= 10
     }
-    return cx.B2s(buf[i:])
+    return cx.B2s(buf.B)
 }
+
+
 
 func Int16ToString(n int16) string {
     if n == 0 {
         return "0"
     }
-    
+
     var buf [6]byte // maximum length of int16 string is 6 digits
     i := len(buf)
     neg := false
@@ -32,14 +34,16 @@ func Int16ToString(n int16) string {
         neg = true
         n = -n
     }
+    bb := bytebufferpool.Get() // get buffer from pool
+    defer bytebufferpool.Put(bb) // return buffer to pool
     for n > 0 {
         i--
-        buf[i] = digits[int(n%10)]
+        bb.WriteByte(digits[int(n%10)])
         n /= 10
     }
     if neg {
         i--
-        buf[i] = '-'
+        bb.WriteByte('-')
     }
-    return cx.B2s(buf[i:])
+    return cx.B2s(bb.Bytes())
 }
